320e1d22a97c8a073321eba02f36369d91199a52
darker <darkerros@yandex.ru>
2023-03-27 21:12:20 +0300

    feat: create search response

---
+++ src/types/responses/search-response.ts
@@ -0,0 +1,7 @@
+import {BookVolumeResource} from "../resources/book-volume-resource";
+
+export interface SearchResponse {
+    kind: "books#volumes",
+    items: BookVolumeResource[],
+    totalItems: number
+}

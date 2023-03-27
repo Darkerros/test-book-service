06b17012e150ba4882e98d6943cea6aa68dc7677
darker <darkerros@yandex.ru>
2023-03-27 21:12:45 +0300

    feat: create api interface

---
+++ src/types/api-interface.ts
@@ -0,0 +1,9 @@
+import {OrderBy} from "./order-by";
+import {BookCategory} from "./book-category";
+import {SearchResponse} from "./responses/search-response";
+import {GetBookInfoResponse} from "./responses/get-book-info-response";
+
+export interface IApi {
+    search: (query: string, orderBy: OrderBy, category: BookCategory) => Promise<SearchResponse>,
+    getBookInfo: (id: string) => Promise<GetBookInfoResponse>
+}

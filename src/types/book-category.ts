1eeab1db3fbd80ad71810e82a4c76ef49692e3b3
darker <darkerros@yandex.ru>
2023-03-27 21:11:28 +0300

    feat: create orderBy, book category filter enum

---
+++ src/types/book-category.ts
@@ -0,0 +1,9 @@
+export enum BookCategory {
+    all = "",
+    art = "art",
+    biography = "biography",
+    computers = "computers",
+    history = "history",
+    medical = "medical",
+    poetry = "poetry",
+}

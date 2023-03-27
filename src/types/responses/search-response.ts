5a3d6262ce391670d0cca41d38807d5c4cb89786
darker <darkerros@yandex.ru>
2023-03-28 00:56:01 +0300

    fix: fix search response wheare response items undefined

--- src/types/responses/search-response.ts
+++ src/types/responses/search-response.ts
@@ -2,6 +2,6 @@ import {BookVolumeResource} from "../resources/book-volume-resource";
 
 export interface SearchResponse {
     kind: "books#volumes",
-    items: BookVolumeResource[],
+    items?: BookVolumeResource[],
     totalItems: number
 }

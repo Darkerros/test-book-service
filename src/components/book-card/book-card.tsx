309534d583163f8cce27e9b1ceb80a2940bb1334
darker <darkerros@yandex.ru>
2023-03-28 22:27:02 +0300

    fix: fix if book card poster is null

--- src/components/book-card/book-card.tsx
+++ src/components/book-card/book-card.tsx
@@ -1,6 +1,7 @@
 import styles from './book-card.module.scss'
 import {BookVolumeResource} from "../../types/resources/book-volume-resource";
 import {FC, memo} from "react";
+import {BookIcon} from "../../ui/icons/book-icon";
 
 interface IBookCardProps {
     bookInfo: BookVolumeResource;
@@ -9,11 +10,16 @@ interface IBookCardProps {
 export const BookCard:FC<IBookCardProps> = memo(({bookInfo}) => {
     const isCategoriesNotNull = bookInfo.volumeInfo.categories && bookInfo.volumeInfo.categories.length
     const isAuthorsNotNull = bookInfo.volumeInfo.authors && bookInfo.volumeInfo.authors.length
+    const isThumbnailsNull = !bookInfo.volumeInfo.imageLinks?.thumbnail
+    console.log(bookInfo.volumeInfo.imageLinks)
 
     return (
         <div className={styles.bookCard}>
             <div className={styles.bookCard__posterContainer}>
-                <img className={styles.bookCard__posterContainerPoster} src={bookInfo.volumeInfo.imageLinks.thumbnail} alt=""/>
+                {isThumbnailsNull
+                    ? <BookIcon className={styles.bookCard__posterContainerNoPosterIcon}/>
+                    :
+                    <img className={styles.bookCard__posterContainerPoster} src={bookInfo.volumeInfo.imageLinks.thumbnail} alt=""/>}
             </div>
             {isCategoriesNotNull && <p className={styles.bookCard__category}>{bookInfo.volumeInfo.categories[0]}</p>}
             <p className={isCategoriesNotNull ? styles.bookCard__name : `${styles.bookCard__name} ${styles.bookCard__name_category_null}`}>{bookInfo.volumeInfo.title}</p>

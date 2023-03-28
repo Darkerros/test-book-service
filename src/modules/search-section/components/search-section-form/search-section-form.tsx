c89aa23ac770a98faa593e5e9a018d6a02949b09
darker <darkerros@yandex.ru>
2023-03-28 20:18:51 +0300

    feat: create search section form

---
+++ src/modules/search-section/components/search-section-form/search-section-form.tsx
@@ -0,0 +1,23 @@
+import {FC, FormEvent, useCallback} from 'react';
+import styles from "./search-section-from.module.scss"
+import {FilterBlock} from "../../../filter-block";
+import {SearchSectionInput} from "../search-section-input/search-section-input";
+
+
+interface ISearchSectionFormProps {
+    submitHandler: (event: FormEvent) => void
+}
+
+export const SearchSectionForm:FC<ISearchSectionFormProps> = ({submitHandler}) => {
+    const onSubmit = useCallback((event: FormEvent) => {
+        submitHandler(event)
+        // eslint-disable-next-line
+    },[])
+
+    return (
+        <form className={styles.searchSectionForm} onSubmit={onSubmit}>
+            <SearchSectionInput/>
+            <FilterBlock/>
+        </form>
+    );
+};

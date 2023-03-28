a2391e26f1cc4c29c7edbb117f70e6cf3fb1bbad
darker <darkerros@yandex.ru>
2023-03-28 22:35:44 +0300

    feat: add page routin to main

--- src/modules/app/components/app/app.tsx
+++ src/modules/app/components/app/app.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import {Route, Routes} from "react-router-dom";
+import {Layout} from "../../../../components/layout/layout";
+import {MainPage} from "../../../../pages/main-page/main-page";
+import {BookPage} from "../../../../pages/book-page/book=page";
+
 
 export const App = () => {
-  return (
-      <div>
-        
-      </div>
-  );
+
+    return (
+        <Routes>
+            <Route path={"/"} element={<Layout/>}>
+                <Route index element={<MainPage/>}/>
+                <Route path={"/book/:id"} element={<BookPage/>}/>
+            </Route>
+        </Routes>
+    );
 };
 

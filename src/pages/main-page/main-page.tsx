1d2c1d38fe227b9129caaefac2d3a24f24859c67
darker <darkerros@yandex.ru>
2023-03-28 22:28:57 +0300

    feat: complete main page

---
+++ src/pages/main-page/main-page.tsx
@@ -0,0 +1,26 @@
+import styles from './main-page.module.scss'
+import {useAppSelector} from "../../hooks/use-app-selector";
+import {BookCardList} from "../../components/book-card-list/book-card-list";
+import {LoadMoreButton} from "../../components/load-more-button/load-more-button";
+import {useCallback} from "react";
+import {useAppDispatch} from "../../hooks/use-app-dispatch";
+import {getNextSearchPageThunk} from "../../services/thunks/get-next-search-page-thunk";
+import {LoadIcon} from "../../ui/icons/load-icon";
+export const MainPage = () => {
+    const dispatch = useAppDispatch()
+    const {isLoadingNextPage, isLoadingSearch, isSuccessSearch, items: books, total, isHaveNextPage} = useAppSelector(state => state.searchReducer)
+
+    const handleNextSearchPage = useCallback(() => {
+        dispatch(getNextSearchPageThunk())
+        // eslint-disable-next-line
+    },[])
+
+    return (
+        <section className={styles.mainPage}>
+            {isSuccessSearch && <p className={styles.mainPage__foundCount}>Found books: {total}</p>}
+            {isLoadingSearch && <LoadIcon className={styles.mainPage__loader}/>}
+            <BookCardList books={books}/>
+            {isHaveNextPage && <LoadMoreButton isLoading={isLoadingNextPage} extraClass={styles.mainPage__loadMoreBtn} onClick={handleNextSearchPage}/>}
+        </section>
+    );
+};
